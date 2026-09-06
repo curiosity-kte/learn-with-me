00bfbbc497c4540e838fc80d19a57e5c9a211134
Ryan Thomas <curiosityKTE@gmail.com>
2026-09-06 15:41:46 -0400

    Created Powershell version of NVD API Call for CVEs

---
+++ NVD-API-Call.ps1
@@ -0,0 +1,8 @@
+$baseURL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+$cveID = "CVE-2013-3900"
+
+$fullURL = "$($baseURL)?cveID=$cveID"
+
+$response = Invoke-RestMethod -Uri $fullURL
+
+$response.vulnerabilities.cve
